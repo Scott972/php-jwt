9d11bb222733c83c69a487ef15bc01be2490dc53
Scott972 <emmanuelscott972@gmail.com>
2021-11-09 11:20:36 -0600

    Update volumeAttachment.ps1

--- volumeAttachment.ps1
+++ volumeAttachment.ps1
@@ -1,9 +1,23 @@
 param(
-[string]$label
+[string]$cnum
+[string]$domain
 )
+
 $d = Get-Disk | Where-Object partitionstyle -eq 'RAW'
-$driveLetter = 'F'
+$driveLetter = 'G'
 $d |
     Initialize-Disk -PartitionStyle GPT -PassThru |
     New-Partition -UseMaximumSize -DriveLetter $driveLetter |
-    Format-Volume -FileSystem NTFS -NewFileSystemLabel "$label"  -Force
+    Format-Volume -FileSystem NTFS -NewFileSystemLabel "$cnum-DATA"  -Force
+
+New-Item -Path "F:\" -Name "$cnum-UserProfiles" -ItemType "directory"
+
+$NewAcl = Get-Acl -Path "F:\$cnum-UserProfiles"
+
+$identity = "$domain\Domain Users"
+$fileSystemRights = "Modify"
+$type = "Allow"
+$fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
+$fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
+$NewAcl.SetAccessRule($fileSystemAccessRule)
+Set-Acl -Path "F:\$cnum-UserProfiles" -AclObject $NewAcl
